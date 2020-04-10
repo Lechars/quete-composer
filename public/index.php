8387def6a599d5b3af74c3e4d5c44f3ed0ad822b
Charles <charles.chevreuil@gmail.com>
2020-04-10 22:25:18 +0200

    le commit composer deux

--- public/index.php
+++ public/index.php
@@ -1,10 +1,16 @@
-<?php
+cd ..cdcom<?php
 
 
 require '../vendor/autoload.php';
 
-use App\Wcs\Hello;
+//use App\Wcs\Hello;
 
-$hello = new Hello();
+//$hello = new Hello();
 
-echo $hello->talk();
+//echo $hello->talk();
+
+use HelloWorld\SayHello;
+
+ $helloKitty = new SayHello();
+
+ echo $helloKitty->world();
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -9,7 +9,7 @@ class ComposerStaticInit71065423057b374974cd743be380a40c
     public static $prefixLengthsPsr4 = array (
         'A' => 
         array (
-            'App\\' => 5,
+            'App\\' => 4,
         ),
     );
 
@@ -20,11 +20,22 @@ class ComposerStaticInit71065423057b374974cd743be380a40c
         ),
     );
 
+    public static $prefixesPsr0 = array (
+        'H' => 
+        array (
+            'HelloWorld' => 
+            array (
+                0 => __DIR__ . '/..' . '/ehime/hello-world/src',
+            ),
+        ),
+    );
+
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
             $loader->prefixLengthsPsr4 = ComposerStaticInit71065423057b374974cd743be380a40c::$prefixLengthsPsr4;
             $loader->prefixDirsPsr4 = ComposerStaticInit71065423057b374974cd743be380a40c::$prefixDirsPsr4;
+            $loader->prefixesPsr0 = ComposerStaticInit71065423057b374974cd743be380a40c::$prefixesPsr0;
 
         }, null, ClassLoader::class);
     }
